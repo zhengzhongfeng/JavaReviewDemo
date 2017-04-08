eca76c6e457550d8c36244d888074c6e8d8fa143
zhengzhongfeng <903035483@qq.com>
2017-04-08 23:04:52 +0800

    222

--- src/edu/gdin/git/GitTest.java
+++ src/edu/gdin/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("git test111");
+		System.out.println("git test222");
 	}
 }
